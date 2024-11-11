3cf832ba39bb10efe3a7774b5e8e52a443e78e1d
FloRul <rumiel.florian@gmail.com>
2024-11-11 08:54:56 -0500

    add more files

---
+++ main_textract.py
@@ -0,0 +1,12 @@
+﻿import boto3
+from textractcaller import call_textract, Textract_Features
+from textractprettyprinter.t_pretty_print import get_text_from_layout_json
+import json
+import os
+
+
+def main():
+    textract_json = call_textract(input_document="code_batiment_2022.pdf"
+
+if __name__ == '__main__':
+    main()
